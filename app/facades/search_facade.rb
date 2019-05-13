70623313ad4f42b0af22fcb847f14ec6f4fc0405
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-13 10:03:52 -0600

    Adds verbose api consume method

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -6,8 +6,18 @@ class SearchFacade
 
   def results
     conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?") do |f|
+      f.params["api_key"] = ENV["DEV_API_KEY"]
+      f.params["access"] = "public"
+      f.params["fuel_type"] = "ELEC,LPG"
+      f.params["location"] = @zip
+      f.params["radius"] = 5.0
       f.adapter Faraday.default_adapter
     end
 
+    response = conn.get
+    data = JSON.parse(response.body, symbolize_names: true)
+    data[:results].map do |result|
+      Result.new(data)
+    end
   end
 end
