03511c4ecd7fa2e0a1cf848c6a24602daae99f89
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-13 10:58:16 -0600

    Formats service spec for results

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -2,9 +2,10 @@ require "rails_helper"
 
 describe SearchService do
   it "gets results" do
+    zip = 80206
     service = SearchService.new(zip)
-    results = service.get_results
+    results = service.get_stations
     expect(results).to be_an(Array)
-    expect(results).to have_key(:distance)
+    expect(results.first).to have_key(:distance)
   end
 end
